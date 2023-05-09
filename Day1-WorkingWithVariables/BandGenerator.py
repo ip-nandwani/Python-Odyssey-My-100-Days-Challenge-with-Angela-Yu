076795692ee5929e664f176f89dba8307d341a88
Pradeep Nandwani <nandwani.ind@gmail.com>
2023-05-09 23:05:01 +0530

    Day 1

---
+++ Day1-WorkingWithVariables/BandGenerator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band Generator!\nThis tool will help you decide your band name...")
+city = input("Enter the city you grew up in ? ")
+pet = input("Enter the name of the pet you had ? ")
+bandName = city + " " + pet
+print(bandName)
